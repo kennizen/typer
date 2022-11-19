3de43943d947ccd184603953817bcb3bd151e82d
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-11-19 21:47:12 +0530

    fixed usedKeyPress, updated appjs

--- src/App.tsx
+++ src/App.tsx
@@ -3,77 +3,84 @@ import data from "./data/db.json";
 import { useKeyPress } from "./hooks/useKeyPress";
 
 function App() {
-    const text = data[0].text.toLowerCase();
+  const text = data[0].text.toLowerCase();
+  const [matrix, setMatrix] = useState<string[][]>([]);
+  const [currentChar, setCurrentChar] = useState<string>("0-0");
+  const keyPressed = useKeyPress();
 
-    const [matrix, setMatrix] = useState<string[][]>([]);
-    const [currentChar, setCurrentChar] = useState<string>("0-0");
-    const key = useKeyPress();
+  useEffect(() => {
+    if (keyPressed.key === "" || null || undefined) return;
+    const [i, j] = currentChar.split("-");
+    if (
+      keyPressed.key === " " &&
+      matrix[parseInt(i)].length - 1 === parseInt(j)
+    )
+      setCurrentChar((prev) => {
+        const [i, j] = prev.split("-");
+        return (parseInt(i) + 1).toString() + "-" + "0";
+      });
+    else {
+      const [i, j] = currentChar.split("-");
+      if (matrix[parseInt(i)].length - 1 === parseInt(j)) return;
+      setCurrentChar((prev) => {
+        const [i, j] = prev.split("-");
+        return i + "-" + (parseInt(j) + 1).toString();
+      });
+    }
+  }, [keyPressed]);
 
-    useEffect(() => {
-        if (key === "" || null || undefined) return;
-        const [i, j] = currentChar.split("-");
-        if (key === " " && matrix[parseInt(i)].length - 1 === parseInt(j))
-            setCurrentChar((prev) => {
-                const [i, j] = prev.split("-");
-                return (parseInt(i) + 1).toString() + "-" + "0";
-            });
-        else {
-            const [i, j] = currentChar.split("-");
-            if (matrix[parseInt(i)].length - 1 === parseInt(j)) return;
-            setCurrentChar((prev) => {
-                const [i, j] = prev.split("-");
-                return i + "-" + (parseInt(j) + 1).toString();
-            });
-        }
-    }, [key]);
+  useEffect(() => {
+    const arr: string[][] = [];
+    const wordArr = text.split(" ");
 
-    useEffect(() => {
-        const arr: string[][] = [];
-        const wordArr = text.split(" ");
+    wordArr.forEach((wrd) => {
+      const tmp = [];
+      for (let i = 0; i < wrd.length; i++) {
+        tmp.push(wrd[i]);
+      }
+      arr.push(tmp);
+    });
 
-        wordArr.forEach((wrd) => {
-            const tmp = [];
-            for (let i = 0; i < wrd.length; i++) {
-                tmp.push(wrd[i]);
-            }
-            arr.push(tmp);
-        });
+    setMatrix(arr);
+  }, []);
 
-        setMatrix(arr);
-    }, []);
-
-    return (
+  return (
+    <div
+      style={{
+        padding: "5rem",
+        width: "100%",
+        display: "flex",
+        columnGap: "0.5rem",
+        flexWrap: "wrap",
+        fontSize: "40px",
+        color: "GrayText",
+      }}
+    >
+      {matrix.map((str: string[], i: number) => (
         <div
-            style={{
-                padding: "5rem",
-                width: "100%",
-                display: "flex",
-                columnGap: "0.5rem",
-                flexWrap: "wrap",
-                fontSize: "40px",
-                color: "GrayText",
-            }}>
-            {matrix.map((str: string[], i: number) => (
-                <div
-                    key={i}
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                    }}>
-                    {str.map((l: string, j: number) => (
-                        <div
-                            key={j}
-                            style={{
-                                color:
-                                    currentChar === i.toString() + "-" + j.toString() ? "red" : "",
-                            }}>
-                            {l}
-                        </div>
-                    ))}
-                </div>
-            ))}
+          key={i}
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          {str.map((l: string, j: number) => (
+            <div
+              key={j}
+              style={{
+                color:
+                  currentChar === i.toString() + "-" + j.toString()
+                    ? "red"
+                    : "",
+              }}
+            >
+              {l}
+            </div>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
-
+import { useEffect, useState, } from "react";
 export const useKeyPress = () => {
-    const [keyPressed, setKeyPressed] = useState<string>("");
+    const [keyPressed, setKeyPressed] = useState<{ key: string, counter: number }>({ key: '', counter: 0 });
 
     function handleKeyPress(e: KeyboardEvent) {
-        setKeyPressed(e.key);
+        setKeyPressed(prev => ({ key: e.key, counter: prev.counter + 1 }));
     }
 
     useEffect(() => {
